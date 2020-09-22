2d8b0a5e9e7bad2c47cd69d62424ddc284b90609
iamarjun <arjunmanoj1995@gmail.com>
2020-09-22 18:53:20 +0530

    Initial commit

---
+++ app/src/main/java/com/arjun/xcamerax/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.arjun.xcamerax
+
+import android.content.Intent
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        launch_camera.setOnClickListener {
+            dispatchTakePictureIntent()
+        }
+
+        launch_video.setOnClickListener {
+            dispatchTakeVideoIntent()
+        }
+    }
+
+    val REQUEST_IMAGE_CAPTURE = 1
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+    val REQUEST_VIDEO_CAPTURE = 1
+
+    private fun dispatchTakeVideoIntent() {
+        Intent(MediaStore.ACTION_VIDEO_CAPTURE).also { takeVideoIntent ->
+            takeVideoIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+}
